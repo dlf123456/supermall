562e9e8ab27897c6c03563c8ea95daf82ba8f38a
linfeng <1793749031@qq.com>
2020-06-05 17:29:40 +0800

    提交代码

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,39 +1,161 @@
 <template>
   <div id="home">
-    <NavBar class="home-nav"><div slot="center">??</div></NavBar> 
+    <NavBar class="home-nav"><div slot="center">购物街</div></NavBar> 
     <home-swiper :banners="banners"></home-swiper> 
+    <recommend-view :recommends="recommends"></recommend-view>
+    <home-feature-view></home-feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']">
+    
+    </tab-control>
+    <ul>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+      <li>1</li>
+    </ul>
   </div>
 </template>
  <script>
   import NavBar from 'components/common/navbar/NavBar.vue'
+  import TabControl from 'components/content/tabControl/TabControl.vue'
+
   import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView.vue'
+  import HomeFeatureView from './childComps/HomeFeatureView.vue'
   import {getHomeMultiData} from 'network/home'
   
  export default {
    name: "Home",
    components: {
      NavBar,
-     HomeSwiper
+     HomeSwiper,
+     RecommendView,
+     HomeFeatureView,
+     TabControl
    },
    data() {
      return{
        banners: [],
-       recommend:[]
+       recommends:[]
      }
    },
    created() {
      getHomeMultiData().then(res => {
        console.warn(res);
        this.banners = res.data.banner.list;
-       this.recommends = res.data.recommend;
+       this.recommends = res.data.recommend.list;
      })
    }
  }
  </script>
 
  <style scoped>
+  #home{
+    padding-top:44px ;
+  }
   .home-nav{
     background-color: var(--color-tint);
     color:#fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top:100px;
   }
  </style>
