a9cf482200dd4d5101e89a14b6372c8c1702a096
dlf123456 <1793749031@qq.com>
2020-06-22 21:55:24 +0800

    gengxin

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-      {{iid}}
-  </div>
+  <div id="detail">{{iid}}</div>
 </template>
 
 <script>
 export default {
-    name: "Detail",
-    data() {
-        return {
-            iid: null
-        }
-    },
-    created() {
-        this.iid = this.$route.params.iid
-    }
-}
+  name: "Detail",
+  data() {
+    return {
+      iid: null
+    };
+  },
+  created() {
+    this.iid = this.$route.params.iid;
+  }
+};
 </script>
 
 <style>
-
 </style>
